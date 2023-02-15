1afdf8099f38951e137b846ab32b1e727f116b9f
gayatri0803 <85298488+gayatri0803@users.noreply.github.com>
2023-02-15 23:13:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,18 @@
 
 var btn=document.getElementById("btn");
 var numPtag=document.getElementById("num");
+var res=document.getElementById("respond");
+var inputvalue=document.getElementById("guess").value;
+var diff=Number(inputvalue)-Number(numPtag);
 
+if(Math.abs(diff)>=5)
+{
+	res.textContent="Hot";
+}
+else
+{
+	res.textContent="Cold";
+}
 function generateRandomNumber() {
 	var no=Math.floor(Math.random()*40)-20;
 	numPtag.textContent=no;
